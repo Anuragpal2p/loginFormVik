3bd9af5e52fb249df049afbe0f28f981923b41e3
Anuragpal2p <112432540+Anuragpal2p@users.noreply.github.com>
2023-12-18 01:58:40 +0530

    form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,37 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    username: "",
+    pass: "",
+  });
+
+  const handleloginData = (e) => {
+    e.preventDefault();
+    setLoginData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+    console.log(loginData, "myloginData");
+
+    
+  };
+
+  const handleLogin = (e) => {
+   
+    e.preventDefault();
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+
+    if( storedUser.username === loginData.username && storedUser.pass === loginData.pass){
+        alert("You Logged In Successfully!!!!..")
+    }
+    else(
+        alert("Incorrect Credentials")
+       
+    )
+  };
+
   return (
     <>
       <div>
@@ -10,24 +41,33 @@ const Login = () => {
             border: "1px solid grey",
             margin: " 50px auto",
             padding: "50px",
-            fontFamily:"monospace"
+            fontFamily: "monospace",
           }}
+          onSubmit={handleLogin}
         >
-            <h1 style={{fontFamily:"monospace"}}>Login Here!!</h1>
-      <label>Username : </label>    <input
+          <h1 style={{ fontFamily: "monospace" }}>Login Here!!</h1>
+          <label>Username : </label>{" "}
+          <input
             type="text"
             placeholder="Your username"
-            style={{ padding: "4px",   fontFamily:"monospace" }}
+            style={{ padding: "4px", fontFamily: "monospace" }}
+            onChange={handleloginData}
+            name="username"
+            value={loginData.username}
           />
           <br />
           <br />
-       <label>Password : </label>   <input
+          <label>Password : </label>{" "}
+          <input
             type="password"
             placeholder="Your password"
-            style={{ padding: "4px" ,   fontFamily:"monospace"}}
+            style={{ padding: "4px", fontFamily: "monospace" }}
+            onChange={handleloginData}
+            name="pass"
+            value={loginData.pass}
           />
           <br />
-          <br/>
+          <br />
           <button
             style={{
               color: "white",
@@ -35,12 +75,13 @@ const Login = () => {
               border: "none",
               borderRadius: "10px",
               padding: " 8px 62px",
-              fontFamily:"monospace"
+              fontFamily: "monospace",
             }}
+            onClick={handleLogin}
           >
             Login
           </button>
-          <h5 style={{  fontFamily:"monospace", fontWeight:"bold"}}>
+          <h5 style={{ fontFamily: "monospace", fontWeight: "bold" }}>
             Not Ragistered yet? <Link to="/"> Sign Up here</Link>
           </h5>
         </form>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,114 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignUp = () => {
+  const [data, setData] = useState({
+    fullname: "",
+    username: "",
+    pass: "",
+    cpass: "",
+  });
+
+  const [isSign, setIsSign] = useState(true);
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target
+    setData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+  };
+  console.log(data, "my fomrData");
+
+  const navigate = useNavigate();
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    if (data.pass === data.cpass) {
+      alert("Thansk!!! You are Succesfully Registred");
+
+      localStorage.setItem('user', JSON.stringify(data))
+
+      navigate("/login");
+    } else {
+      alert("Password and confirm pass must same");
+    }
+  };
+
   return (
     <>
       <div>
-        <form style={{  fontFamily:"monospace",width:"400px",border:"1px solid grey", margin:" 50px auto", padding:"50px"}}>
-
-          <input type="text" placeholder="Your Fullname"  style={{padding:"4px"}}/> <br/><br/>
-          <input type="text" placeholder="Your username"  style={{padding:"4px"}} /><br/><br/>
-          <input type="password" placeholder="Your password"  style={{padding:"4px"}} /><br/><br/>
-          <input type="password" placeholder="Confirm password"   style={{padding:"4px"}} /><br/><br/>
-          <button style={{  fontFamily:"monospace",color:"white", background:"blue", border:"none", borderRadius:"10px", padding:" 8px 62px"}}>Sign Up</button>
-        <h6 style={{  fontFamily:"monospace"}}>Already Ragistered?<Link to="/login">Login here</Link></h6>
+        <form
+          style={{
+            fontFamily: "monospace",
+            width: "400px",
+            border: "1px solid grey",
+            margin: " 50px auto",
+            padding: "50px",
+          }}
+          onSubmit={handleSignUp}
+        >
+          <input
+            type="text"
+            placeholder="Your Fullname"
+            style={{ padding: "4px" }}
+            onChange={handleInputChange}
+            value={data.fullname}
+            name="fullname"
+            required
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Your username"
+            style={{ padding: "4px" }}
+            onChange={handleInputChange}
+            name="username"
+            value={data.username}
+            required
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            placeholder="Your password"
+            style={{ padding: "4px" }}
+            onChange={handleInputChange}
+            name="pass"
+            value={data.pass}
+            required
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            placeholder="Confirm password"
+            style={{ padding: "4px" }}
+            onChange={handleInputChange}
+            name="cpass"
+            value={data.cpass}
+            required
+          />
+          <br />
+          <br />
+          <button
+            style={{
+              fontFamily: "monospace",
+              color: "white",
+              background: "blue",
+              border: "none",
+              borderRadius: "10px",
+              padding: " 8px 62px",
+            }}
+            type="submit"
+            onClick={handleSignUp}
+          >
+            Sign Up
+          </button>
+          <h6 style={{ fontFamily: "monospace" }}>
+            Already Ragistered?<Link to="/login">Login here</Link>
+          </h6>
         </form>
       </div>
     </>
